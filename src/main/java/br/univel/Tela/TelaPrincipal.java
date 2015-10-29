4d64a7965f74a6cc51f2d79cec804849d48e9ea4
Maycon <mayconvluz@gmail.com>
2015-10-29 20:39:53 -0200

    Criaçao Tela Cadastro Usuario

--- src/main/java/br/univel/Tela/TelaPrincipal.java
+++ src/main/java/br/univel/Tela/TelaPrincipal.java
@@ -10,14 +10,18 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.function.Function;
 
 public class TelaPrincipal extends JFrame {
 
 	private JPanel contentPane;
 	private JTabbedPane tabbedPane;
+	private BlockPanel glass;
 
 	/**
 	 * Launch the application.
@@ -39,7 +43,9 @@ public class TelaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaPrincipal() {
-		setTitle("Sistema de Cadastro");
+
+		blockParaLogin();
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 
@@ -57,6 +63,14 @@ public class TelaPrincipal extends JFrame {
 
 		});
 		mnCadastros.add(mntmCliente);
+		
+		JMenuItem mntmBloquear = new JMenuItem("BLOQUEAR");
+		mntmBloquear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				block();
+			}
+		});
+		mnCadastros.add(mntmBloquear);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -66,10 +80,51 @@ public class TelaPrincipal extends JFrame {
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 	}
 
+	protected void block() {
+		setGlassPane(glass);
+		glass.setVisible(true);
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				for (int i =0; i < 5; i++) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				glass.setVisible(false);
+			}
+		}).start();
+	}
+
+	private void blockParaLogin() {
+		Runnable acaoOk = () -> {
+			glass.setVisible(false);
+			glass = new BlockPanel();
+		};
+
+		PainelLogin painelLogin = new PainelLogin(acaoOk);
+
+		glass = new BlockPanel(painelLogin);
+
+		setGlassPane(glass);
+
+		glass.setVisible(true);
+	}
+
 	private void abrirTela() {
 		TelaCadastroCliente telaCadastroCliente = new TelaCadastroCliente();
-		telaCadastroCliente.setCloseAction(e -> tabbedPane
-				.remove(telaCadastroCliente));
+		ActionListener action = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.remove(telaCadastroCliente);
+			}
+		};
+		telaCadastroCliente.setCloseAction(action);
+
 		tabbedPane.addTab("Tela ", telaCadastroCliente);
 	}
 
